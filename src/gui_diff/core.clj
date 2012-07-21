38058ad3b4dc96bda20896818449fc6047b23329
Alex Baranosky <alex@runa.com>
2012-07-21 20:07:05 +0530

    first commit

---
+++ src/gui_diff/core.clj
@@ -0,0 +1,1 @@
+(ns gui-diff.core)
---
+++ test/gui_diff/test/core.clj
@@ -0,0 +1,6 @@
+(ns gui-diff.test.core
+  (:use [gui-diff.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
