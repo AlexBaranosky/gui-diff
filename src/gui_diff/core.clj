260a26fc4a8665c8bc36c340c2380f0e628b9a09
Alex Baranosky <alex@runa.com>
2012-07-22 13:33:50 +0530

    first working version of gui-diff

--- src/gui_diff/core.clj
+++ src/gui_diff/core.clj
@@ -1,1 +1,37 @@
-(ns gui-diff.core)
+(ns gui-diff.core
+  (:require [clojure.pprint :as pp]
+            [clojure.java.shell :as sh])
+  (:import java.io.File))
+
+(defn- map-values
+  "Apply a function on all values of a map and return the corresponding map (all keys untouched)"
+  [f m]
+  (zipmap
+    (keys m)
+    (map f (vals m))))
+
+(defn- nested-sort [x]
+  (cond (sequential? x)
+    (if (instance? java.lang.Comparable (first x))
+      (sort (map nested-sort x))
+      (map nested-sort x))
+
+    (map? x)
+    (if (and (not= {} x)
+             (instance? java.lang.Comparable (key (first x))))
+      (into (sorted-map) (map-values nested-sort x))
+      (map-values nested-sort x))
+
+    :else))
+
+(defn gui-diff
+  "Display a visual diff of two data structures, using Mac's FileMerge tool."
+  [a b]
+  (let [file-a (File. "/tmp/a.txt")
+        file-b (File. "/tmp/b.txt")
+        a-pp (with-out-str (pp/pprint (nested-sort (read-string a))))
+        b-pp (with-out-str (pp/pprint (nested-sort (read-string b))))]
+    (spit file-a a-pp)
+    (spit file-b b-pp)
+
+    (sh/sh "opendiff" "/tmp/a.txt" "/tmp/b.txt")))
