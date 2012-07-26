fb5520de2c6f5a80d755fbab3102ef6078950fd8
Alex Baranosky <alex@runa.com>
2012-07-27 01:42:48 +0530

    added test of the spacing code

--- src/gui_diff/core.clj
+++ src/gui_diff/core.clj
@@ -66,7 +66,7 @@
     (diff-files file-1 file-2)))
 
 (defn num-lines [s]
-  (count (str/split s #"\\n")))
+  (count (str/split s #"\n")))
 
 (defn pad-with-extra-lines [s n]
   (if (zero? n)
@@ -159,6 +159,10 @@ FAIL in (test-fail-more) (NO_SOURCE_FILE:67)
 expected: (= {:A 1} {:a 1, :b 2, :c 3, :d 4, :e 5})
   actual: (not (= {:A 1} {:a 1, :c 3, :b 2, :d 4, :e 5}))")
 
+(def different-heights-FAIL "FAIL in (test-fail-high) (NO_SOURCE_FILE:67)
+expected: (= {:A 1} {:a 1, :b 2, :c 3, :d 4, :e 5})
+  actual: (not (= {:A 1} {:a 11111, :c 33333777776666622222921347128472847124871472340, :b 2, :d 4, :e 55555}))")
+
 (ct/deftest test-1
   (ct/is (= [{:test-name "test-fail"
               :file-info "NO_SOURCE_FILE:6"
@@ -176,3 +180,13 @@ expected: (= {:A 1} {:a 1, :b 2, :c 3, :d 4, :e 5})
               :expected "1\n"
               :actual "2\n"}]
             (ct-report-str->failure-maps single-FAIL))))
+
+(ct/deftest test-33
+  (ct/is (= [{:test-name "test-fail-high"
+              :file-info "NO_SOURCE_FILE:67"
+              :expected "{:A 1}\n\n \n \n \n "
+              :actual "{:a 11111,\n :b 2,\n :c 33333777776666622222921347128472847124871472340N,\n :d 4,\n :e 55555}\n"}]
+            (ct-report-str->failure-maps different-heights-FAIL))))
+
+;; TODO: test exps/acts/ with differenet lengths/heights space properly in the
+;; diff report
