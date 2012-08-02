c678c43b50fcc26ea34c7b071cfbcc1be47e419b
Alex Baranosky <alex@runa.com>
2012-08-02 23:36:41 +0530

    upped version to 0.3.5 - fixed bug in nested-sort for keys with different types

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gui-diff "0.3.4"
+(defproject gui-diff "0.3.5"
   :description "Visual diffing of Clojure data structures"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [ordered "1.2.0"]])
