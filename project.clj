521df6129e41a11d227206d0f8fa7ba0ef14bb12
Alex Baranosky <alexander.baranosky@gmail.com>
2013-01-07 21:44:10 -0800

    moved everything into gui=diff.core and improved doc-strings

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gui-diff "0.3.9"
+(defproject gui-diff "0.4.0-SNAPSHOT"
   :description "Visual diffing of Clojure data structures"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [ordered "1.2.0"]])
