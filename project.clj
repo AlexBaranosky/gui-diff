f01c9b2d0ae06c274f539cfb11e6c90b7da429d5
Alex Baranosky <alexander.baranosky@gmail.com>
2013-01-27 00:57:49 -0800

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gui-diff "0.4.0-SNAPSHOT"
+(defproject gui-diff "0.4.0"
   :description "Visual diffing of Clojure data structures"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojars.trptcolin/sjacket "0.1.3" :exclusions [org.clojure/clojure]]
