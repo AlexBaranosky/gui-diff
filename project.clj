e61bcd9a34388816efb15c1ffb885e66ca561ce3
Torkus <48141663+ogri-la@users.noreply.github.com>
2020-06-14 23:27:09 +0930

    bumps org.flatland/ordered to 1.5.9 fixes flatland issue #45 with java 11 https://github.com/clj-commons/ordered/issues/45

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [org.clojure/clojure]]
                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
-                 [org.flatland/ordered "1.5.3"]]
+                 [org.flatland/ordered "1.5.9"]]
   :profiles {:1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
